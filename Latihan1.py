28257899fbe38c2c3f01e87ad07541021baaa131
alfizainulhaq <alfizainulhaq@gmail.com>
2025-10-30 16:07:39 +0700

    latihan1

---
+++ Latihan1.py
@@ -0,0 +1,16 @@
+from random import random
+
+# Meminta input dari user
+n = int(input("Masukkan nilai N: "))
+
+i = 0
+count = 0
+
+# Gunakan while untuk mengulang hingga jumlah data = n
+while count < n:
+    a = random()
+    # hanya tampilkan bila a < 0.5
+    if a < 0.5:
+        count += 1
+        print(f"data ke: {count} => {a}")
+print("Selesai")
